638ed2c6223870e4df8d0f6e5fe91943dd2d8d88
Jeffrey Kim <12345678+jeffreyykim@users.noreply.github.com>
2025-10-08 14:32:27 -0700

    - Added DataField class with specifications

---
+++ src/main/model/DataField.java
@@ -0,0 +1,35 @@
+package model;
+
+
+/*
+Represents a single field definition within a data set
+name represents the label of the data
+type represents the type of values generated
+*/
+
+
+public class DataField {
+    private String name; 
+    private String type; 
+
+    // REQUIRES: name and type are not empty 
+    // EFFECTS: constructs a datafield with the given name and type
+    public DataField(String name, String type){
+
+    }
+
+    // EFFECTS: returns the name of this datafield
+    public String getName(){
+        return null;
+    }
+
+    // EFFECTS: returns the type of this datafield
+    public String getType() {
+        return null;
+    }
+
+    // EFFECTS: generates a random value based on the type of field
+    public String generateValue() {
+        return null;
+    }
+}
