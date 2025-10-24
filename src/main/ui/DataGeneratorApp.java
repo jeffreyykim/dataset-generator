6bdebd350240dd7fcf0d4e571e0abe254bf07b2b
Jeffrey Kim <12345678+jeffreyykim@users.noreply.github.com>
2025-10-24 15:33:19 -0700

    - Finalized DataGeneratorApp with save and load features for data persistence

--- src/main/ui/DataGeneratorApp.java
+++ src/main/ui/DataGeneratorApp.java
@@ -2,17 +2,30 @@ package ui;
 
 import model.DataField;
 import model.DataSet;
+import persistence.JsonReader;
+import persistence.JsonWriter;
+
 import java.util.List;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
 
 // Mock data generator application
+@ExcludeFromJacocoGeneratedReport
 public class DataGeneratorApp {
     private DataSet dataset;
     private Scanner input;
+    private static final String JSON_STORE = "./data/dataset.json";
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
 
     // EFFECTS: runs the data generator application
     public DataGeneratorApp() {
         input = new Scanner(System.in);
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
         runApp();
     }
 
@@ -49,11 +62,16 @@ public class DataGeneratorApp {
             listFields();
         } else if (command.equals("g")) {
             generateData();
+        } else if (command.equals("s")) {
+            saveDataSet();
+        } else if (command.equals("o")) {
+            loadDataSet();
         } else {
             System.out.println("Selection not valid...");
         }
     }
 
+    // EFFECTS: displays menu of options to user
     private void displayMenu() {
         System.out.println("\nSelect from:");
         System.out.println("\tc -> create dataset");
@@ -61,9 +79,13 @@ public class DataGeneratorApp {
         System.out.println("\tr -> remove field");
         System.out.println("\tl -> list fields");
         System.out.println("\tg -> generate data");
+        System.out.println("\ts -> save dataset as JSON");
+        System.out.println("\to -> load dataset from JSON");
         System.out.println("\tq -> quit");
     }
 
+    // MODIFIES: this
+    // EFFECTS: creates a new dataset
     private void createDataSet() {
         System.out.println("Enter dataset name: ");
         String name = input.nextLine();
@@ -71,6 +93,8 @@ public class DataGeneratorApp {
         System.out.println("Dataset \"" + name + "\" created.");
     }
 
+    // MODIFIES: this
+    // EFFECTS: adds a new field to the current dataset
     private void addField() {
         if (dataset == null) {
             System.out.println("Please create a dataset first.");
@@ -84,6 +108,8 @@ public class DataGeneratorApp {
         }
     }
 
+    // MODIFIES: this
+    // EFFECTS: removes a field from the current dataset by name
     private void removeField() {
         if (dataset == null) {
             System.out.println("Please create a dataset first.");
@@ -106,6 +132,7 @@ public class DataGeneratorApp {
         }
     }
 
+    // EFFECTS: displays all fields in the current dataset
     private void listFields() {
         if (dataset == null) {
             System.out.println("Please create a dataset first.");
@@ -117,6 +144,7 @@ public class DataGeneratorApp {
         }
     }
 
+    // EFFECTS: generates mock data for all fields in the dataset
     private void generateData() {
         if (dataset == null) {
             System.out.println("Please create a dataset first.");
@@ -127,6 +155,33 @@ public class DataGeneratorApp {
             for (int i = 0; i < fields.size(); i++) {
                 System.out.println(fields.get(i).getName() + ": " + data.get(i));
             }
+        }
+    }
+
+    // EFFECTS: saves the current dataset to file
+    private void saveDataSet() {
+        if (dataset == null) {
+            System.out.println("Please create a dataset first.");
+            return;
+        }
+        try {
+            jsonWriter.open();
+            jsonWriter.write(dataset);
+            jsonWriter.close();
+            System.out.println("Saved " + dataset.getName() + " to " + JSON_STORE);
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to write to file " + JSON_STORE);
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads dataset from file
+    private void loadDataSet() {
+        try {
+            dataset = jsonReader.read();
+            System.out.println("Loaded " + dataset.getName() + " from " + JSON_STORE);
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + JSON_STORE);
         }
     }
 }
