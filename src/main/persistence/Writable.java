2f4da65b33526846201031928ef8f0e3846a944c
Jeffrey Kim <12345678+jeffreyykim@users.noreply.github.com>
2025-10-24 15:38:35 -0700

    - Added class level comment to Writable

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -2,6 +2,9 @@ package persistence;
 
 import org.json.JSONObject;
 
+/*
+ * Represents an object that can be written to a JSON file
+ */
 public interface Writable {
     // EFFECTS: returns this as JSON object
     JSONObject toJson();
