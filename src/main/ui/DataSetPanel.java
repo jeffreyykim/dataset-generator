7dde610d88e92715a016c4daf30e688e32b42d52
Jeffrey Kim <12345678+jeffreyykim@users.noreply.github.com>
2025-11-20 09:36:39 -0800

    Added DataSetPanel class

---
+++ src/main/ui/DataSetPanel.java
@@ -0,0 +1,45 @@
+package ui;
+
+import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
+import model.DataField;
+import model.DataSet;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+/*
+ * Panel that displays all DataFields currently in a DataSet.
+ * Each field is shown on its own line.
+ */
+@ExcludeFromJacocoGeneratedReport
+public class DataSetPanel extends JPanel {
+    private DataSet dataSet;
+    private DefaultListModel<String> listModel;
+    private JList<String> fieldList;
+
+    // EFFECTS: constructs panel that displays fields from given DataSet
+    public DataSetPanel(DataSet dataSet) {
+        this.dataSet = dataSet;
+
+        setLayout(new BorderLayout());
+        setBorder(BorderFactory.createTitledBorder("Fields in Dataset"));
+
+        listModel = new DefaultListModel<>();
+        fieldList = new JList<>(listModel);
+
+        add(new JScrollPane(fieldList), BorderLayout.CENTER);
+        refresh();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: reloads the list of fields from the DataSet
+    public void refresh() {
+        listModel.clear();
+        List<DataField> fields = dataSet.getFields();
+        for (DataField f : fields) {
+            String line = f.getName() + " (" + f.getType() + ")";
+            listModel.addElement(line);
+        }
+    }
+}
